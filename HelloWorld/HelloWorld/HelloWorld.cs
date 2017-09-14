8473c23289bc1d6259d789e8fb8f9977b02c6504
Barry King <brry.kng@gmail.com>
2017-09-14 18:26:19 +0800

    Hello World added

---
+++ HelloWorld/HelloWorld/HelloWorld.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            /* First console application in C# */
+            Console.WriteLine("First console application in C#");
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
+        }
+    }
+}
